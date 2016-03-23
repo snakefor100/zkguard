3d8b04ea9897683d784770d4ccaf227b0eba5d7d
niujunlong <540973185@qq.com>
2016-03-24 00:22:47 +0800

    init

--- zkguard-dao/src/main/java/com/junlong/zkguard/dao/ZkClusterInfoDao.java
+++ zkguard-dao/src/main/java/com/junlong/zkguard/dao/ZkClusterInfoDao.java
@@ -1,10 +1,12 @@
 package com.junlong.zkguard.dao;
 
 import com.junlong.zkguard.domain.ZkClusterInfo;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by niuniu on 2016/3/23.
  */
+@Repository
 public interface ZkClusterInfoDao {
     void saveClusterInfo(ZkClusterInfo zkClusterInfo);
 }
